212e8b34170d89e5f1b3d481b6997be672a9aaf0
RafaelPaulo <rafael-sq@hotmail.com>
2017-01-25 22:39:57 -0200

    CRUD's templates and components

---
+++ src/components/taskForm.js
@@ -0,0 +1,45 @@
+import Ntask from '../ntask.js'
+import Template from '../templates/taskForm.js'
+
+class taskForm extends Ntask {
+    constructor(body) {
+        super()
+        this.body = body
+    }
+    render() {
+        this.body.innerHTML = Template.render()
+        this.body.querySelector('[data-task]')
+        this.addEventListener()
+    }
+    addEventListener() {
+        this.formSubmit()
+    }
+    formSubmit() {
+        const form = this.body.querySelector('form')
+        form.addEventListener('submit', (e) => {
+            e.preventDefault()
+            const task = e.target.querySelector('[data-task]')
+            const opts = {
+                method: 'POST',
+                url: `${this.URL}/tasks`,
+                json: true,
+                header: {
+                    authorization: localStorage.getItem('token')
+                },
+                body: {
+                    title: task.value
+                }
+            }
+            this.request(opts, (err, resp, data) => {
+                if(err || resp.status === 412) {
+                    this.emit('error')
+                }
+                else {
+                    this.emit('submit')
+                }
+            })
+        })
+    }
+}
+
+module.exports = TaskForm
